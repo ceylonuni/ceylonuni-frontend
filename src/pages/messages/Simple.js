ded996099df7d4c2f7e994b49ca9e6d6876163e6
unknown <josiah.prathaban@gmail.com>
2022-06-20 11:55:50 +0530

    finish and close CEYF-1

--- src/pages/messages/Simple.js
+++ src/pages/messages/Simple.js
@@ -7,7 +7,7 @@ export default function Message() {
       <div className="flex flex-col justify-center items-center px-10 md:mt-0 mt-12">
         <img src={logo} className="max-w-xs" alt="logo" />
         <p className="text-xl font-medium text-gray-500 px-5 mt-5 max-w-lg text-center">
-          A verification link has been sent to <b className="text-green-500">example@course.uni.lk</b>. Click that link to verify your email and registr to <b className="text-teal-500">ceylonuni.lk</b>.
+          A verification link has been sent to <b className="text-green-500">exampleuser@course.uni.lk</b>. Click that link to verify your email and registr to <b className="text-teal-500">ceylonuni.lk</b>.
         </p>
       </div>
     </div>
